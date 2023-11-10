f297fcacade57fc6c5cd891b7fdbf1b39f07583f
Laman Khudadatzada <lkhudadatzada@gmail.com>
2023-11-10 11:11:54 +0400

    "build gradle"

---
+++ src/main/java/com/nnamanx/skylink/SkylinkApplication.java
@@ -0,0 +1,13 @@
+package com.nnamanx.skylink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkylinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkylinkApplication.class, args);
+    }
+
+}
