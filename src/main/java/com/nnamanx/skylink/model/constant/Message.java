dde18cb7d68040dabd7d3f127a736a7cba04614f
Laman Khudadatzada <lkhudadatzada@gmail.com>
2023-11-11 15:32:23 +0400

    "User Request & response & Constant Messages"

---
+++ src/main/java/com/nnamanx/skylink/model/constant/Message.java
@@ -0,0 +1,13 @@
+package com.nnamanx.skylink.model.constant;
+
+public class Message {
+
+    public static final String EMAIL_IS_URGENT = "Enet your email";
+    public static final String EMAIL_IS_NOT_VALID = "Email is not valid!";
+    public static final String PASSWORD_IS_NOT_VALID = "Enter a password";
+    public static final String PASSWORD_REGEX = "Password is not in acceptable format!";
+    public static final String USERNAME_IS_URGENT = "Enter a Username";
+    public static final String PASSPORT_FIN_IS_URGENT = "Enter your passport fin";
+    public static final String PASSPORT_SERIA_IS_URGENT = "Enter your passport seria";
+}
+
---
+++ src/main/java/com/nnamanx/skylink/model/dto/response/UserResponse.java
@@ -0,0 +1,19 @@
+package com.nnamanx.skylink.model.dto.response;
+
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserResponse {
+
+    String name;
+    String email;
+
+}
