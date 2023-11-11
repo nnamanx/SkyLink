065ce9ff456cac82499eb46106f32ac2dd20c50f
Laman Khudadatzada <lkhudadatzada@gmail.com>
2023-11-11 20:15:03 +0400

    "OAuth 2.0 Configuration"

--- src/main/java/com/nnamanx/skylink/config/OAuth2Config.java
+++ src/main/java/com/nnamanx/skylink/config/OAuth2Config.java
@@ -1,53 +1,53 @@
-package com.nnamanx.skylink.config;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-
-@Configuration
-@EnableAuthorizationServer
-@RequiredArgsConstructor
-public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
-
-    private AuthenticationManager authenticationManager;
-
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients
-                .inMemory()
-                .withClient("client-id")
-                .secret("{noop}client-secret") // In a real application, use proper password encoding
-                .authorizedGrantTypes("password", "refresh_token")
-                .scopes("read", "write")
-                .accessTokenValiditySeconds(3600)
-                .refreshTokenValiditySeconds(7200);
-    }
-
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints
-                .authenticationManager(authenticationManager)
-                .tokenStore(tokenStore())
-                .accessTokenConverter(accessTokenConverter());
-    }
-
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(accessTokenConverter());
-    }
-
-    @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setSigningKey("jwt-signing-key");
-        return converter;
-    }
-}
+//package com.nnamanx.skylink.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+//import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+//import org.springframework.security.oauth2.provider.token.TokenStore;
+//import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+//import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+//
+//@Configuration
+//@EnableAuthorizationServer
+//@RequiredArgsConstructor
+//public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+//
+//    private AuthenticationManager authenticationManager;
+//
+//    @Override
+//    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//        clients
+//                .inMemory()
+//                .withClient("client-id")
+//                .secret("{noop}client-secret") // In a real application, use proper password encoding
+//                .authorizedGrantTypes("password", "refresh_token")
+//                .scopes("read", "write")
+//                .accessTokenValiditySeconds(3600)
+//                .refreshTokenValiditySeconds(7200);
+//    }
+//
+//    @Override
+//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+//        endpoints
+//                .authenticationManager(authenticationManager)
+//                .tokenStore(tokenStore())
+//                .accessTokenConverter(accessTokenConverter());
+//    }
+//
+//    @Bean
+//    public TokenStore tokenStore() {
+//        return new JwtTokenStore(accessTokenConverter());
+//    }
+//
+//    @Bean
+//    public JwtAccessTokenConverter accessTokenConverter() {
+//        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+//        converter.setSigningKey("jwt-signing-key");
+//        return converter;
+//    }
+//}
--- src/main/java/com/nnamanx/skylink/config/WebSecurityConfig.java
+++ src/main/java/com/nnamanx/skylink/config/WebSecurityConfig.java
@@ -3,34 +3,26 @@ package com.nnamanx.skylink.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private UserDetailsService userDetailsService;
-
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-    }
+public class WebSecurityConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.antMatchers("/").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .oauth2Client(withDefaults())
+                .formLogin(withDefaults())
+                .build();
     }
 }
