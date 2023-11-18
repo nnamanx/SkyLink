69c80638a9658ddca9abce343a51a7b52c5582d6
Laman Khudadatzada <lkhudadatzada@gmail.com>
2023-11-18 20:41:25 +0400

    "Exception Response"

---
+++ src/main/java/com/nnamanx/skylink/model/dto/response/ExceptionResponse.java
@@ -0,0 +1,19 @@
+package com.nnamanx.skylink.model.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ExceptionResponse {
+
+    String message;
+    LocalDateTime timestamp;
+}
