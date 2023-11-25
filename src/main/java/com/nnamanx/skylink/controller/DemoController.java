e290ceb7ea95311b142bd63fcf22701ee7b26617
Laman Khudadatzada <lkhudadatzada@gmail.com>
2023-11-25 21:45:50 +0400

    "Demo Controller"

--- src/main/java/com/nnamanx/skylink/controller/DemoController.java
+++ src/main/java/com/nnamanx/skylink/controller/DemoController.java
@@ -1,26 +1,18 @@
 package com.nnamanx.skylink.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
-@Slf4j
 @RequiredArgsConstructor
-public class HomeController {
+@RequestMapping("/api/v1/demo-controller")
+public class DemoController {
 
-// http://localhost:8080/login/oauth2/github
-    @GetMapping("/")
-    public String home() {
-        return "Hello, Home!";
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello from secured endpoint");
     }
-
-    @GetMapping("/secured")
-    public String secured() {
-        return "Hello, Secured!";
-    }
-
 }
