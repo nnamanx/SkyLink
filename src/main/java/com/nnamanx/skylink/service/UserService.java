3810e8e0e24a0831fac2e3cc9b6f8a5d03d33f28
Laman Khudadatzada <lkhudadatzada@gmail.com>
2023-11-26 11:45:58 +0400

    "Update password error handling"

--- src/main/java/com/nnamanx/skylink/service/UserService.java
+++ src/main/java/com/nnamanx/skylink/service/UserService.java
@@ -1,14 +1,16 @@
 package com.nnamanx.skylink.service;
 
 import com.nnamanx.skylink.model.entity.EndUser;
+import org.springframework.http.ResponseEntity;
 
+import java.util.Map;
 import java.util.Optional;
 
 public interface UserService {
 
     boolean isValidResetToken(String username, String token);
 
-    void updatePassword(String username, String newPassword);
+    ResponseEntity<Map<String, String>> updatePassword(String username, String newPassword);
 
     Optional<EndUser> findByUsername(String username);
 
--- src/main/java/com/nnamanx/skylink/service/impl/UserServiceImpl.java
+++ src/main/java/com/nnamanx/skylink/service/impl/UserServiceImpl.java
@@ -4,10 +4,13 @@ import com.nnamanx.skylink.model.entity.EndUser;
 import com.nnamanx.skylink.repository.UserRepository;
 import com.nnamanx.skylink.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -25,7 +28,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public void updatePassword(String username, String newPassword) {
+    public ResponseEntity<Map<String, String>> updatePassword(String username, String newPassword) {
         Optional<EndUser> optionalUser = userRepository.findByUsername(username);
 
         if (optionalUser.isPresent()) {
@@ -36,8 +39,16 @@ public class UserServiceImpl implements UserService {
 
             user.setRefresh_token(null);
             userRepository.save(user);
+
+            Map<String, String> response = new HashMap<>();
+            response.put("message", "Password updated successfully");
+            return ResponseEntity.ok(response);
         } else {
-            // Handle the case where the user is not found
+
+            Map<String, String> response = new HashMap<>();
+            response.put("message", "User not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+
         }
     }
 
