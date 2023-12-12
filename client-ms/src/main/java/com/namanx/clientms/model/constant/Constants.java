283699493d6aa348d002fc4dcc3389d3f69e0002
Laman Khudadatzada <lkhudadatzada@gmail.com>
2023-12-12 23:16:16 +0400

    "client-ms: Client Request and its constants"

---
+++ client-ms/src/main/java/com/namanx/clientms/model/constant/Constants.java
@@ -0,0 +1,12 @@
+package com.namanx.clientms.model.constant;
+
+public class Constants {
+
+    public final static String ENTER_USER_NAME = "Enter a username.";
+    public final static String ENTER_EMAIL = "Enter an email address.";
+    public final static String ENTER_PASSWORD = "Enter a password";
+    public final static String ENTER_PHONE_NUMBER = "Enter a phone number.";
+    public final static String INVALID_EMAIL = "Email is not valid.";
+    public final static String INVALID_PASSWORD = "Password should contain at least 8 characters, one uppercase letter and one number.";
+    public final static String INVALID_BIRTHDATE = "The date is not valid.";
+}
---
+++ client-ms/src/main/java/com/namanx/clientms/model/dto/request/ClientRequest.java
@@ -0,0 +1,43 @@
+package com.namanx.clientms.model.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+import java.time.LocalDateTime;
+
+import static com.namanx.clientms.model.constant.Constants.*;
+
+@Builder
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ClientRequest {
+
+
+    @NotBlank(message = ENTER_USER_NAME)
+    String username;
+
+    @NotBlank(message = ENTER_EMAIL)
+    @Email(message = INVALID_EMAIL)
+    String email;
+
+    @NotBlank(message = ENTER_PASSWORD)
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\\\d).{8,}$",
+            message = INVALID_PASSWORD)
+    String password;
+
+    @NotBlank(message = ENTER_PHONE_NUMBER)
+    @Pattern(regexp = "^[+\\d][\\d\\s()-]*$", message = "Invalid phone number format")
+    String phoneNumber;
+
+    String fin;
+    String serialNumber;
+
+    @Past(message = INVALID_BIRTHDATE) // whether the date is in the past
+    LocalDateTime birthdate;
+
+}
