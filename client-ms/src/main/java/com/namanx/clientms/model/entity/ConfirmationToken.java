6bc911c310b24a8fb6534923982575c17d114931
Laman Khudadatzada <lkhudadatzada@gmail.com>
2023-12-12 22:42:11 +0400

    "client-ms: token entities"

--- client-ms/src/main/java/com/namanx/clientms/model/entity/ConfirmationToken.java
+++ client-ms/src/main/java/com/namanx/clientms/model/entity/ConfirmationToken.java
@@ -1,4 +1,27 @@
 package com.namanx.clientms.model.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ConfirmationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String confirmationToken;
+    LocalDateTime expirationDate;
 }
