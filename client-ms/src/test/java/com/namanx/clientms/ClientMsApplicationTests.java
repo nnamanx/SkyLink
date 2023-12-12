c78379af6586ebeabd0d85c959750fffded52bb3
Laman Khudadatzada <lkhudadatzada@gmail.com>
2023-12-12 20:16:14 +0400

    "creating client-ms"

---
+++ client-ms/src/test/java/com/namanx/clientms/ClientMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namanx.clientms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
