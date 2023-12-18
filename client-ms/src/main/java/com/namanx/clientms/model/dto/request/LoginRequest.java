1c751a293f6d535f3a9133e79b1a6f199d54a23c
Laman Khudadatzada <lkhudadatzada@gmail.com>
2023-12-18 23:30:58 +0400

    "client-ms: Login Request"

--- client-ms/src/main/java/com/namanx/clientms/model/dto/request/LoginRequest.java
+++ client-ms/src/main/java/com/namanx/clientms/model/dto/request/LoginRequest.java
@@ -1,7 +1,6 @@
 package com.namanx.clientms.model.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
