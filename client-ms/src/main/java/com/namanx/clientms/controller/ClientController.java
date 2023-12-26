7981ad16aeca6a98d87fc544815fcbafe95bbba9
Laman Khudadatzada <lkhudadatzada@gmail.com>
2023-12-27 00:07:26 +0400

    "initializing the controller for client"

---
+++ client-ms/src/main/java/com/namanx/clientms/controller/ClientController.java
@@ -0,0 +1,13 @@
+package com.namanx.clientms.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/client-ms")
+@RequiredArgsConstructor
+@Slf4j
+public class ClientController {
+}
