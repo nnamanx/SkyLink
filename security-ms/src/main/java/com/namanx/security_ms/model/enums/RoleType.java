6b9267553bc696c6fc3204680c9b48c756239e40
Laman Khudadatzada <lkhudadatzada@gmail.com>
2023-12-29 19:01:11 +0400

    "defining token type and role types"

---
+++ security-ms/src/main/java/com/namanx/security_ms/model/enums/RoleType.java
@@ -0,0 +1,7 @@
+package com.namanx.security_ms.model.enums;
+
+public enum RoleType {
+
+    ADMIN,
+    CLIENT
+}
---
+++ security-ms/src/main/java/com/namanx/security_ms/model/enums/TokenType.java
@@ -0,0 +1,6 @@
+package com.namanx.security_ms.model.enums;
+
+public enum TokenType {
+
+    BEARER
+}
