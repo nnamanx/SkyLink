d53dbc604bb10c2367df55480722fe406b14105d
Laman Khudadatzada <lkhudadatzada@gmail.com>
2023-12-29 18:23:23 +0400

    "building gradle for security microservice"

---
+++ security-ms/src/test/java/com/namanx/security_ms/SecurityMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namanx.security_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
