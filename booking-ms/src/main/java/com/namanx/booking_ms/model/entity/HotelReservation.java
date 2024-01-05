ee3e2df2f6999565508e3d1ad79bea86faba2af4
Laman Khudadatzada <lkhudadatzada@gmail.com>
2024-01-05 22:52:44 +0400

    "hotel reservation entity"

---
+++ booking-ms/src/main/java/com/namanx/booking_ms/model/entity/HotelReservation.java
@@ -0,0 +1,30 @@
+package com.namanx.booking_ms.model.entity;
+
+import com.namanx.booking_ms.model.enums.RoomType;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+public class HotelReservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String hotelName;
+    @Enumerated(EnumType.STRING)
+    RoomType roomType;
+    LocalDateTime checkInDateTime;
+    LocalDateTime checkOutDateTime;
+    Double price;
+
+    // Long client_id
+
+}
---
+++ booking-ms/src/main/java/com/namanx/booking_ms/model/enums/RoomType.java
@@ -0,0 +1,14 @@
+package com.namanx.booking_ms.model.enums;
+
+public enum RoomType {
+
+    SINGLE,
+    DOUBLE,
+    QUEEN,
+    KING,
+    TRIPLE,
+    SUITE,
+    PRESIDENTIAL_SUITE,
+    FAMILY,
+    STUDIO
+}
